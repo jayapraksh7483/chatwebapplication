092ae5692d763c205241df0f5e483a0a53fe050b
divvi jayaprakash <divvijayaprakash@example.com>
2025-05-25 19:47:17 +0530

    m

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -18,7 +18,9 @@ app.use(cors({
   credentials: true,
 }));
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.resolve();
+ 
+const __dirname  = path.dirname(__filename);
+
 // PORT should be assigned after calling dotenv.config() because we need to access the env variables. Didn't realize while recording the video. Sorry for the confusion.
 const PORT = process.env.PORT || 5000;
 
@@ -29,10 +31,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.use(express.static(path.join(__dirname, "frontend", "dist")));
+app.use(express.static(path.join(__dirname, "frontend")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "frontend", "index.html"));
 });
 
 server.listen(PORT, () => {
