692297e7531b5d8391fbc61bf4b1df0506ae292f
divvi jayaprakash <divvijayaprakash@example.com>
2025-05-26 08:45:58 +0530

    m

--- FRONTEND/vite.config.ts
+++ FRONTEND/vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
     },
   },
   preview: {
-    allowedHosts: ['chatwebapplication-5.onrender.com'], // Add your backend host here
+    allowedHosts: ['chatwebapplication-7.onrender.com'], // Add your backend host here
   },
 });
 
