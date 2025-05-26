d12b2ca4a336be7a4c6557750a22ed46a683988d
divvi jayaprakash <divvijayaprakash@example.com>
2025-05-26 20:27:14 +0530

    Fix backend build script and root build scripts

--- BACKEND/utills/generateToken.js
+++ BACKEND/utills/generateToken.js
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 
-const generateTokenandSetCookie = (userId,res)=>{
+const  generateTokenandSetCookie= (userId,res)=>{
    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
   expiresIn: "15d",
 });
