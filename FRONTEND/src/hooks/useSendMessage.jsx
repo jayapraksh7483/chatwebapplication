6f23c57252ca2e3046658dabff67f61a577f7e23
divvi jayaprakash <divvijayaprakash@example.com>
2025-05-26 10:47:09 +0530

    m

--- FRONTEND/src/hooks/useSendMessage.jsx
+++ FRONTEND/src/hooks/useSendMessage.jsx
@@ -16,6 +16,7 @@ const useSendMessage = () => {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
+                     "Authorization": `Bearer ${token}`,
                    
                 },
                 body: JSON.stringify({message}),
