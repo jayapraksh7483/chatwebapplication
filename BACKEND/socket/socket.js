6d04887dad73b68709757e3ea8e7b8bb1febfe18
divvi jayaprakash <divvijayaprakash@example.com>
2025-05-26 08:56:25 +0530

    m

--- BACKEND/socket/socket.js
+++ BACKEND/socket/socket.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
-		origin: [ "http://localhost:5173"],
+		origin: [ " https://chatwebapplication-6.onrender.com"],
 		methods: ["GET", "POST"],
 		 
 	},
