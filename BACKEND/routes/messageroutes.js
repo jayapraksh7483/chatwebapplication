361b7cb6673837481d68d03f7aa18eee9e65e0c2
divvi jayaprakash <divvijayaprakash@example.com>
2025-05-27 16:23:55 +0530

    m

--- BACKEND/routes/messageroutes.js
+++ BACKEND/routes/messageroutes.js
@@ -1,11 +1,11 @@
 import express from "express";
 import { sendmessage,getmessage } from "../controllers/messagecontrollers.js";
- import protectRoute from "../middleware/protectroute.js";
+ 
 
 const router = express.Router();
 
-router.get("/:id", protectRoute , getmessage);
-router.post("/send/:id", protectRoute , sendmessage);
+router.get("/:id",   getmessage);
+router.post("/send/:id" , sendmessage);
  
 
 export default router;
