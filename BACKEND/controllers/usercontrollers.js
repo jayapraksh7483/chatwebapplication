a4d673abbf3c71b44c24d516aa63ff3598481963
divvi jayaprakash <divvijayaprakash@example.com>
2025-05-27 16:58:57 +0530

    m

--- BACKEND/controllers/usercontrollers.js
+++ BACKEND/controllers/usercontrollers.js
@@ -1,14 +1,19 @@
 import User from "../models/usermodel.js";
 
-
 export const getUsersForSidebar = async (req, res) => {
   try {
-    const loggedInuserId = req.user._id; // Get the logged-in user's ID from the request
-    const filteredUsers = await User.find({ _id: { $ne: loggedInuserId } }).select("-password") // Find all users except the logged-in user
- 
+    console.log("User from request:", req.user);
+    const loggedInuserId = req.user?._id;
+
+    if (!loggedInuserId) {
+      return res.status(400).json({ message: "User ID not found in request" });
+    }
+
+    const filteredUsers = await User.find({ _id: { $ne: loggedInuserId } }).select("-password");
+
     res.status(200).json(filteredUsers);
   } catch (err) {
-    console.error(err);
+    console.error("Error in getUsersForSidebar:", err);
     res.status(500).json({ message: "Internal server error" });
   }
-}
+};
