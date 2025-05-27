653ccdb79c1a39ac4d6bd9f47896e24cce128fd8
divvi jayaprakash <divvijayaprakash@example.com>
2025-05-27 16:46:23 +0530

    m

--- BACKEND/controllers/authcontrollers.js
+++ BACKEND/controllers/authcontrollers.js
@@ -30,7 +30,7 @@ export const signup = async (req, res) => {
     });
 
     await newUser.save(); // Save first
-    generateTokenandSetCookie(newUser._id, res); // then generate token
+   // then generate token
 
     res.status(201).json({
       _id: newUser._id,
@@ -56,8 +56,7 @@ export const signup = async (req, res) => {
     if (!user || !isPasswordCorrect) {
       return res.status(400).json({ error: "Invalid username or password" });
     }
-
-    generateTokenandSetCookie(user._id, res);
+ 
 
     res.status(200).json({
       _id: user._id,
