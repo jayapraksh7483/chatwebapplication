89b3985b059640b0aaf02feb791118f283b1f8e3
divvi jayaprakash <divvijayaprakash@example.com>
2025-05-28 15:12:38 +0530

    m

--- FRONTEND/src/pages/homepage/Homepage.jsx
+++ FRONTEND/src/pages/homepage/Homepage.jsx
@@ -4,7 +4,7 @@ import MessagesContainer from '../../components/Messages/MessagesContainer'
 
 const Homepage = () => {
   return (
-    <div className='flex min-h-screen w-auto bg-green border border-blue-400 rounded-lg shadow-lg bg-gray-900'>
+    <div className='flex sm:h-[450px] md:h-[550px] bg-green border border-blue-400 rounded-lg shadow-lg bg-gray-900'>
 
      <Sidebar/>
       <MessagesContainer/>  
