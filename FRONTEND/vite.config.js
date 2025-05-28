e00f7c6ea9b5e6346d456b630b87397b661942f0
divvi jayaprakash <divvijayaprakash@example.com>
2025-05-28 11:06:30 +0530

    m

--- FRONTEND/vite.config.js
+++ FRONTEND/vite.config.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
+
   
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tailwindcss(), react()],
   server: {
     port: 5173,
     proxy: {
