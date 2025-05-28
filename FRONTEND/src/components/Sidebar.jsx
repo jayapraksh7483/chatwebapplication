cf37dc01bd762e057102838b79d517f682049b68
divvi jayaprakash <divvijayaprakash@example.com>
2025-05-28 12:48:24 +0530

    m

--- FRONTEND/src/components/Sidebar.jsx
+++ FRONTEND/src/components/Sidebar.jsx
@@ -6,7 +6,7 @@ import LogoutButton from './LogoutButton'
 
 const Sidebar = () => {
   return (
-    <div className='border-r border-slate-500 p-4 flex flex-col min-h-screen'>
+    <div className='border-r border-slate-500 p-4 flex flex-col'>
         <SearchInput/>
         <div className="divider px-3"></div>
          <Conservations/>
--- FRONTEND/src/pages/homepage/Homepage.jsx
+++ FRONTEND/src/pages/homepage/Homepage.jsx
@@ -4,7 +4,7 @@ import MessagesContainer from '../../components/Messages/MessagesContainer'
 
 const Homepage = () => {
   return (
-    <div className='flex sm:h-[450px] md:h-[550px] bg-green border border-blue-400 rounded-lg shadow-lg bg-gray-900'>
+    <div className='flex min-h-screen w-full bg-green border border-blue-400 rounded-lg shadow-lg bg-gray-900'>
 
      <Sidebar/>
       <MessagesContainer/>  
